d179c6934a0e7c6f32d3094c89200eee357de751
Haraman Johal <haraman.johal@oxfordni.com>
2020-10-11 19:31:03 +0100

    Updates

---
+++ precache-manifest.5b6661773453a9e1a2a93082b383e44a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656670443f250ced5e55580d56056651",
+    "url": "/charade-cascade/index.html"
+  },
+  {
+    "revision": "a6ccca95f056e581b8a0",
+    "url": "/charade-cascade/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9b64f98aa53e1a937e8",
+    "url": "/charade-cascade/static/js/2.67915964.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charade-cascade/static/js/2.67915964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ccca95f056e581b8a0",
+    "url": "/charade-cascade/static/js/main.18674c48.chunk.js"
+  },
+  {
+    "revision": "bb1c25303a2c6a6e83ea",
+    "url": "/charade-cascade/static/js/runtime-main.e120a22a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charade-cascade/static/media/logo.5d5d9eef.svg"
+  }
+]);
